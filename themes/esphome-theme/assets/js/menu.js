adf3d6496d4570766919724a70c0be50730ea4f8
clydebarrow <2366188+clydebarrow@users.noreply.github.com>
2025-05-27 17:49:34 +1000

    Show subresults in search panel

--- themes/esphome-theme/assets/js/menu.js
+++ themes/esphome-theme/assets/js/menu.js
@@ -157,6 +157,96 @@ document.addEventListener('DOMContentLoaded', function() {
         return;
     }
 
+
+    class El {
+        constructor(tagname) {
+            this.element = document.createElement(tagname);
+        }
+
+        id(s) {
+            this.element.id = s;
+            return this;
+        }
+
+        class(s) {
+            this.element.classList.add(s);
+            return this;
+        }
+
+        attrs(obj) {
+            for (const [k, v] of Object.entries(obj)) {
+                this.element.setAttribute(k, v);
+            }
+            return this;
+        }
+
+        text(t) {
+            this.element.innerText = t;
+            return this;
+        }
+
+        html(t) {
+            this.element.innerHTML = t;
+            return this;
+        }
+
+        handle(e, f) {
+            this.element.addEventListener(e, f);
+            return this;
+        }
+
+        addTo(el) {
+            if (el instanceof El) {
+                el.element.appendChild(this.element);
+            } else {
+                el.appendChild(this.element);
+            }
+            return this.element;
+        }
+    }
+
+    function getLink(location, anchors, url) {
+        if (!anchors || !anchors.length)
+            return null;
+        // find the closest anchor at or before the current location
+        const anchor = anchors.sort((a, b) => b.location - a.location).find(a => a.location <= location);
+        if (anchor) {
+            return url + "#" + anchor.id;
+        }
+        return null;
+    }
+
+    const resultTemplate = (result) => {
+        const wrapper = new El("li").class("pagefind-modular-list-result");
+        wrapper.handle("click", closeResults);
+
+        const thumb = new El("div").class("pagefind-modular-list-thumb").addTo(wrapper);
+        let image = result?.meta?.image;
+        if (image) {
+            if (image.includes("/_images/"))
+                image = image.substring(image.indexOf("/_images/"));
+            new El("img").class("pagefind-modular-list-image").attrs({
+                src: image,
+                alt: result.meta.image_alt || result.meta.title
+            }).addTo(thumb);
+        }
+
+        const inner = new El("div").class("pagefind-modular-list-inner").addTo(wrapper);
+        const title = new El("p").class("pagefind-modular-list-title").addTo(inner);
+        new El("a").class("pagefind-modular-list-link").text(result.meta?.title).attrs({
+            href: result.meta?.url || result.url
+        }).addTo(title);
+
+        const excerpt = new El("p").class("pagefind-modular-list-excerpt").addTo(inner);
+        const locations = result.weighted_locations.sort((a, b) => b.weight - a.weight);
+        const url = getLink(locations[0]?.location, result.anchors, result.url) || result.meta?.url || result.url;
+        new El("a").class("pagefind-modular-list-link").html(result.excerpt).attrs({
+            href: url
+        }).addTo(excerpt);
+
+        return wrapper.element;
+    }
+
     // Create search input and container
     const searchContainer = document.getElementById('nav-search-container');
 
@@ -189,10 +279,17 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Add results component
     instance.add(new PagefindModularUI.ResultList({
-        containerElement: "#nav-search-results"
+        containerElement: "#nav-search-results",
+        resultTemplate: resultTemplate
     }));
 
+
     let top_hit = null;
+
+    function closeResults() {
+        resultsContainer.style.display = 'none';
+        top_hit = null;
+    }
 
     // Show/hide results
     instance.on("results", async (results) => {
@@ -200,16 +297,15 @@ document.addEventListener('DOMContentLoaded', function() {
             resultsContainer.style.display = 'block';
             data = await results.results[0].data();
             top_hit = data.url;
+            console.log(data);
         } else {
-            resultsContainer.style.display = 'none';
-            top_hit = null;
+            closeResults();
         }
     });
 
     document.addEventListener('click', function(e) {
         if (!e.target.closest('#nav-search-results')) {
-            resultsContainer.style.display = 'none';
-            top_hit = null;
+            closeResults();
         }
     });
     // Create clear button
